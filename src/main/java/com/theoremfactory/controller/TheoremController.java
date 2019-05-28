28540b17f82873a1e9f51cce92c5f9b14683264e
skryshneu <serega.maestro.97@gmail.com>
2019-05-28 12:00:00 +0300

    index.html has added

--- src/main/java/com/theoremfactory/controller/TheoremController.java
+++
@@ -1,6 +0,0 @@
-package com.theoremfactory.controller;
-
-
-public class TheoremController {
-
-}
--- src/main/java/com/theoremfactory/dao/BasicDao.java
+++
@@ -1,7 +0,0 @@
-package com.theoremfactory.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BasicDao<T> extends JpaRepository<T, Long> {
-
-}
