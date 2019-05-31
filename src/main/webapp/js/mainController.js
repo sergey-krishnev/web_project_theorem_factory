5ee92965b33821ac2f8114008ba41ac675dbb076
skryshneu <serega.maestro.97@gmail.com>
2019-05-31 15:24:10 +0300

    generation of theorems has added

--- src/main/webapp/js/mainController.js
+++ src/main/webapp/js/mainController.js
@@ -1,5 +1,12 @@
-var mainApp = angular.module('mainApp', []);
-var id;
+let mainApp = angular.module('mainApp', []);
+let functor_id;
+let relation_id;
+let class_id;
+let tmodel_id;
+let functor_entity;
+let relation_entity;
+let class_entity;
+let tmodel_entity;
 mainApp.controller('FunctorCtrl', function ($scope, $http){
     $http.get(location.href + 'functors').success(function(data) {
         $scope.functors = data;
@@ -27,9 +34,10 @@ mainApp.controller('TmodelCtrl', function ($scope, $http) {
 function checkFunctor(entity) {
     event.preventDefault();
     document.getElementById("FunctorDropdown").classList.remove("show");
-    id = entity.getAttribute("href");
-    loadJSON(location.href + 'functors/' + id,
+    functor_id = entity.getAttribute("href");
+    loadJSON(location.href + 'functors/' + functor_id,
         function(data) { console.log(data.functorProperties[0]);
+            functor_entity = data;
             let html = '';
             let renderName = katex.renderToString(data.name, {
                 throwOnError: false
@@ -52,16 +60,15 @@ function checkFunctor(entity) {
 function checkRelation(entity) {
     event.preventDefault();
     document.getElementById("RelationDropdown").classList.remove("show");
-    id = entity.getAttribute("href");
-    loadJSON(location.href + 'relations/' + id,
+    relation_id = entity.getAttribute("href");
+    loadJSON(location.href + 'relations/' + relation_id,
         function(data) { console.log(data.relationProperties[0]);
+            relation_entity = data;
             let html = '';
             let renderName = katex.renderToString(data.name, {
                 throwOnError: false
             });
-            let renderDescription = katex.renderToString(data.description, {
-                throwOnError: false
-            });
+            let renderDescription = renderOddPartly(data.description);
             html +='<li><b>Name:</b> <div lang="latex">'+ renderName +'</div></li>';
             html +='<li><b>Description:</b> '+ renderDescription +' </li>';
             html +='<li><b>Properties:</b></li>';
@@ -80,9 +87,10 @@ function checkRelation(entity) {
 function checkClass(entity) {
     event.preventDefault();
     document.getElementById("ClassDropdown").classList.remove("show");
-    id = entity.getAttribute("href");
-    loadJSON(location.href + 'classes/' + id,
+    class_id = entity.getAttribute("href");
+    loadJSON(location.href + 'classes/' + class_id,
         function(data) { console.log(data.classProperties[0]);
+            class_entity = data;
             let html = '';
             let renderName = katex.renderToString(data.name, {
                 throwOnError: false
@@ -105,9 +113,10 @@ function checkClass(entity) {
 function checkTModel(entity) {
     event.preventDefault();
     document.getElementById("TmodelDropdown").classList.remove("show");
-    id = entity.getAttribute("href");
-    loadJSON(location.href + 'tmodels/' + id,
+    tmodel_id = entity.getAttribute("href");
+    loadJSON(location.href + 'tmodels/' + tmodel_id,
         function(data) { console.log(data);
+            tmodel_entity = data;
             let html = '';
             let renderName = katex.renderToString(data.name, {
                 throwOnError: false
@@ -136,6 +145,52 @@ function checkTModel(entity) {
         function(xhr) { console.error(xhr); }
     );
 }
+
+function generateTemplate() {
+    let html = '';
+    for (let i = 0; i < tmodel_entity.templates.length; i++) {
+        html += '<h3>' + tmodel_entity.templates[i].name + '</h3>';
+
+        let substitute_content = substitute(tmodel_entity.templates[i].content);
+
+        let renderContent = renderOddPartly(substitute_content);
+
+        html += '<p>' + renderContent + '</p>';
+        html += '<hr>';
+    }
+    document.getElementById('generated-file-preview').innerHTML = html;
+}
+
+function substitute(content) {
+
+    let substitute_functor = content.split("NameSpecificFunctor").join(functor_entity.name);
+    return substitute_functor.split("NameSpecificRelation").join(relation_entity.name);;
+
+}
+
+function renderOddPartly(content) {
+    let arrStr = content.split(/[$]/);
+    for (let i = 1; i < arrStr.length; i+=2) {
+        let renderContent = katex.renderToString(arrStr[i], {
+            throwOnError: false
+        });
+        arrStr[i] = renderContent;
+    }
+    return arrStr.join(" ");
+
+}
+
+function renderEvenPartly(content) {
+    let arrStr = content.split(/[$]/);
+    for (let i = 0; i < arrStr.length; i+=2) {
+        let renderContent = katex.renderToString(arrStr[i], {
+            throwOnError: false
+        });
+        arrStr[i] = renderContent;
+    }
+    return arrStr.join(" ");
+}
+
 
 function loadJSON(path, success, error) {
     var xhr = new XMLHttpRequest();
